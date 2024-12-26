b8064816394b9edc67dbc675b05a93fc28756390
bddjr <bddjr08@outlook.com>
2024-12-26 20:54:30 +0800

    first commit

---
+++ Minecraft-Server-Maintaining.py
@@ -0,0 +1,60 @@
+# Config
+ADDR = ("", 25565)
+DESCRIPTION_TEXT = "§c服务器正在维护\nServer Maintaining"
+DESCRIPTION_VERSION = "Server Maintaining"
+CONNECTION_ERROR = "§c服务器正在维护\nServer Maintaining"
+
+# ------
+# Using MIT License.
+# https://github.com/bddjr/Minecraft-Server-Maintaining
+#
+# Tested version:
+#  - Python: 3.12.5
+#  - Minecraft: 1.21.4 1.12.2 1.8.9
+# ------
+
+import socket, socketserver, json
+
+print(f"127.0.0.1:{ADDR[1]}")
+
+description = json.dumps(
+    {
+        "description": {"text": DESCRIPTION_TEXT},
+        "players": {"max": 0, "online": 0},
+        "version": {"name": DESCRIPTION_VERSION, "protocol": 0},
+    }
+).encode("utf-8")
+
+description = bytes([len(description) + 3, 1, 0, len(description), 1]) + description
+
+connectionError = json.dumps(CONNECTION_ERROR).encode("utf-8")
+
+connectionError = (
+    bytes([len(connectionError) + 2, 0, len(connectionError)]) + connectionError
+)
+
+
+class Handler(socketserver.BaseRequestHandler):
+    def handle(self):
+        c: socket.socket = self.request
+        try:
+            c.settimeout(5)
+            packetLen = c.recv(1)[0]
+            data = c.recv(packetLen)
+            match data[-1]:
+                case 1:
+                    # ping
+                    c.recv(2)
+                    c.send(description)
+                    data = c.recv(257)
+                    c.send(data)
+                case 2:
+                    # join
+                    c.recv(257)
+                    c.send(connectionError)
+        finally:
+            c.close()
+
+
+srv = socketserver.ThreadingTCPServer(ADDR, Handler)
+srv.serve_forever()
---
+++ README.md
@@ -0,0 +1,23 @@
+# Minecraft Server Maintaining
+
+伪我的世界Java版服务器，用于表示服务器正在维护。
+
+![](description.jpg)
+
+![](connection-error.jpg)
+
+---
+
+## Start
+
+### Windows
+
+```
+py Minecraft-Server-Maintaining.py
+```
+
+### Linux
+
+```
+python3 Minecraft-Server-Maintaining.py
+```
